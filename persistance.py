d5ef0974b4e180884d9ed223af3b6e4c5002a597
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-01-27 23:29:24 -0600

    Create persistance.py

---
+++ persistance.py
@@ -0,0 +1,9 @@
+from msgspec.json import decode
+from msgspec import Struct
+
+
+"""
+TODO: Finish this file by creating tools for encoding and decoding `Conversation` objects
+to and from json + tools for saving and loading json files locally. Should add config for database
+location / json file location and remove old variables.
+"""
