82501cf6453d0b4a2dea864b2f61f22143096cb7
Samuel Morris <samuel.morris2@mail.mcgill.ca>
2024-01-29 09:28:40 -0600

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,10 +1,17 @@
 import config
 import openai
+from typing import *
 
 
 client = openai.OpenAI(api_key=config.OPENAI_API_KEY)
 
-def stream(conversation, model='gpt-4', **kwargs):
+
+def chat_stream(conversation, model='gpt-4', **kwargs):
+    """
+    Send a chat request to OpenAI and stream back the response.
+    This function returns a generator containing string-chunks,
+    which eventually compose the entire OpenAI response.
+    """
     for chunk in client.chat.completions.create(
         model=model,
         messages=list(conversation),
@@ -12,50 +19,16 @@ def stream(conversation, model='gpt-4', **kwargs):
         **kwargs,
     ):
         if content := chunk.choices[0].delta.content:
-            #print('')
             yield content
 
 
-def create(prompt=None, messages=None, stream=False, model='gpt-4', **kwargs):
+def chat(conversation, model='gpt-4', **kwargs):
     """
-    Handles stream v.s. non-stream creation and prompt v.s. messages creation, while
-    still allowing any arbitrary parameters to enter the API call.
+    Send a chat request to OpenAI and return a string response.
     """
-    create_function = response_stream if stream else _create_flat
-    messages = messages if messages else [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt},
-    ]
-    return create_function(model=model, messages=messages)
-
-
-def _request_completion(*args, **kwargs):
-    """
-    Create a chat-completion using the OpenAI API.
-    """
-    try:
-        return client.chat.completions.create(*args, **kwargs)
-    except openai.APIError as e:
-        print('Error generating GPT completion.')
-
-
-def _create_flat(*args, **kwargs):
-    """
-    Create a chat-completion and return a "flat" string with the result.
-    """
-    try:
-        return _request_completion(*args, **kwargs).choices[0].message.content
-    except Exception as e:
-        print('Error parsing GPT completion.')
-
-
-def response_stream(*args, **kwargs):
-    """
-    Create a chat-completion stream and return a generator yielding the string results.
-    """
-    try:
-        for chunk in client.chat.completions.create(*args, **kwargs, stream=True):
-            if content := chunk.choices[0].delta.content:
-                yield content
-    except Exception as e:
-        raise openai.OpenAIError from e
+    return client.chat.completions.create(
+        model=model,
+        messages=list(conversation),
+        stream=False,
+        **kwargs,
+    ).choices[0].message.content
