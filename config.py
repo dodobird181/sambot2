e3befada8da785d9969ae1ff5ff2f7b4d5c1ca81
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-03-31 14:36:51 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ Global application config.
 """
 
 import logging
+import os
 
 # TODO: Delete old config vars
 CONVO_ID_SESSION_KEY = "sambot_convo_id"
@@ -12,5 +13,5 @@ PICKLE_DB_PATH = ".pkldb/"
 CHAT_SESSION_KEY = "sambot-chat-session"
 LOGGING_LEVEL = logging.INFO
 
-# import secret keys
-from env_secrets import *
+FLASK_SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
