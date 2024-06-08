894cfea541e91b7d0a7707b92910c211b046e5eb
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-06-07 22:10:24 -0400

    growing input box

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world JS!");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const growingInputs = document.getElementsByClassName('growing-input');
+    const textWidthCalculator = document.getElementById('text-width-calculator');
+    for (let i = 0; i < growingInputs.length; i++) {
+        growingInputs[i].addEventListener('input', function() {
+            textWidthCalculator.textContent = this.value;
+            const width = textWidthCalculator.offsetWidth;
+            this.style.width = (width + 20) + 'px'; // Add some padding to avoid too tight fit
+        });
+    }
+});
