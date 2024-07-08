71e038c9c479513f9874026a3ca93973ca0039f1
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-07-08 15:48:44 -0400

    handle environment secrets in production

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,15 @@
-from _env_secrets import FLASK_SECRET_KEY, OPENAI_API_KEY
+import os
 
+# This is the flag that determines whether we are in production
 DEBUG = False
+
+if DEBUG:
+    # use local secrets for development
+    from _env_secrets import FLASK_SECRET_KEY, OPENAI_API_KEY
+else:
+    # pull secrets from production environment
+    FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 SESSION_MESSAGES_KEY = "sambot-messages"
 LOCAL_MESSAGES_DIR = "../data/messages"
